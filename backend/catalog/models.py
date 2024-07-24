8ec9c78247e1199dbbbc14404f59e3f027b19086
Alisson <100442612+alessaocarvalho@users.noreply.github.com>
2024-07-24 12:57:33 -0300

    modelagem do artista e migrations

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Modelagem do Artista
+
+class Artista(models.Model):
+    TIPO_ARTISTA = [
+        ('Banda', 'Banda'),
+        ('Solo', 'Solo'),
+    ]
+    TIPO_DOCUMENTO = [
+        ('CPF', 'CPF'),
+        ('RG', 'RG'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=10, choices=TIPO_ARTISTA)
+    email = models.EmailField()
+    documento = models.CharField(max_length=20)
+    tipo_documento = models.CharField(max_length=3, choices=TIPO_DOCUMENTO)
+
+    def __str__(self) :
+        return self.nome
